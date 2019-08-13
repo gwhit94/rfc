9d06b793fc60a63130934d20ddd19b727e05bc72
Graham <greysnaa@gmail.com>
2019-08-13 11:25:39 -0500

    Set up components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
